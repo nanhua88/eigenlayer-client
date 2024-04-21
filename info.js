bf508566232053810e39003e89de657786a4d385
nanhua <167661653+nanhua88@users.noreply.github.com>
2024-04-21 11:02:08 +0800

    Create info.js

---
+++ info.js
@@ -0,0 +1,7 @@
+{
+"name": "Web3 plugin",
+"website": "https://github.com/nanhua88",
+"description": "developer web3 plugins",
+"logo": "https://raw.githubusercontent.com/nanhua88/eigenlayer-client/main/image.png",
+"twitter": "https://twitter.com/web3"
+}
